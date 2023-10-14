47d1beac3cb8e2203d4fd9bba2c2db0c0e1dadcd
perezandy <perezandy2001@gmail.com>
2023-10-14 11:49:37 -0400

    testing setup functions

---
+++ setup.py
@@ -0,0 +1,12 @@
+setup(
+    name='chatGIT',
+    version='0.1',
+    entry_points={
+        'console_scripts': [
+            'chatGIT = chatGIT.main:main',
+        ],
+    },
+    install_requires=[  # List your package dependencies here
+        # Example: 'requests',
+    ],
+)
