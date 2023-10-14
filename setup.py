139f55ed95988a71b11882d58bfc990b380fe363
perezandy <perezandy2001@gmail.com>
2023-10-14 11:59:02 -0400

    added setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-setup(
+import setuptools
+setuptools.setup(
     name='chatGIT',
     version='0.1',
+    py_modules=['chatGIT'],
     entry_points={
         'console_scripts': [
-            'chatGIT = chatGIT.main:main',
+            'chatGIT = chatGIT:main',
         ],
     },
     install_requires=[  # List your package dependencies here
