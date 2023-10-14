e3c0ebcfeab7c759ee44c514d478591dd7eef52f
perezandy <perezandy2001@gmail.com>
2023-10-14 13:47:16 -0400

    testing setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='chatGIT',
     version='0.1',
     py_modules=['chatGIT'],
-    scripts=['src/chatGIT'],
+    scripts=['/src/chatGIT'],
     entry_points={
         'console_scripts': [
             'chatGIT = chatGIT:main',
